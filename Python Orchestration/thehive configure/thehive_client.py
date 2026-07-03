7547ffc468ad517fcfdf4ac73ae1cf097856216c
Tith Sopanha <sop98886@gmail.com>
2026-07-03 15:35:55 +0700

    Update thehive_client.py

--- Python Orchestration/thehive configure/thehive_client.py
+++ Python Orchestration/thehive configure/thehive_client.py
@@ -2,8 +2,6 @@
 import urllib3
 import requests
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
 class TheHiveClient:
     def __init__(
         self,
@@ -34,9 +32,6 @@ class TheHiveClient:
             return r.status_code == 200
         except Exception:
             return False
-
-
-
     def create_case(self, case_data: dict):
         r = self.session.post(
             f"{self.url}/api/v1/case",
